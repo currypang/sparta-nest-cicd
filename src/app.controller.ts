4918c715af786a8821c06977cf07a3e48e00d39b
romesway <romesway@gmail.com>
2024-07-08 15:40:29 +0900

    3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     const { message, data } = this.appService.getHello();
 
     return {
-      message: 'hello',
+      message,
       data: {
         serverPort: data.serverPort,
         jwtSecret: data.jwtSecret,
